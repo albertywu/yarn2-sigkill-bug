14db6c770cfa93a5ba6243ff05d9e2a1517efd80
Albert Wu <wua@uber.com>
2020-11-19 13:04:46 -0800

    fix comment

--- yarn1-working/repro.sh
+++ yarn1-working/repro.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-yarn --version # confirm we're using yarn v2 berry
+yarn --version # confirm we're using yarn v1
 yarn -s sleep & # run the sleep script in package.json
 sleep 1
 parent_process=$(ps | grep "node.*yarn -s sleep" | grep -v "grep" | awk '{print $1}')
 echo "killing yarn process: $parent_process"
 kill "$parent_process"
 
-# note: at this point, the child sleep process should be terminated but is not
+# note: at this point, the child sleep process is terminated as expected
 if ps | grep 'sleep 1000' | grep -v 'grep'
 then
   echo "[FAIL] still sleeping"
